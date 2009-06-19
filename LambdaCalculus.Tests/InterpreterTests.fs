f638607bc57936af142c61ab366190ab161143d5
holoed_cp <holoed@users.noreply.github.com>
2009-06-19 20:16:20 +0000

    Added Multiplication test 2 * 3

--- LambdaCalculus.Tests/InterpreterTests.fs
+++ LambdaCalculus.Tests/InterpreterTests.fs
@@ -54,4 +54,8 @@ type InterpreterTests =
         
     [<Test>] 
     member o.ThreePlusTwo() = 
-        Assert.AreEqual("(λf.(λx.(f (f (f (f (f x)))))))", interpret "(λm.λn.λf.λx.((m f) (n f x))) (λh.λx.(h (h x))) (λg.λx.(g (g (g x))))")         
+        Assert.AreEqual("(λf.(λx.(f (f (f (f (f x)))))))", interpret "(λm.λn.λf.λx.((m f) (n f x))) (λh.λx.(h (h x))) (λg.λx.(g (g (g x))))")  
+        
+    [<Test>]
+    member o.TwoMultiplyByThree() =
+        Assert.AreEqual("(λf.(λx.(f (f (f (f (f (f x))))))))", interpret "(λm.λn.λf.(n (m f))) (λh.λx.(h (h x))) (λg.λx.(g (g (g x))))")       
